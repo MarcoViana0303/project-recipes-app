a5f14e90c8d6155e56aa9338e5ab89be8ded1e41
queones <queones@gmail.com>
2022-10-24 18:40:54 -0300

    Pagina login feita - erros de lint

--- src/context/appProvider.js
+++ src/context/appProvider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './appContext';
 
