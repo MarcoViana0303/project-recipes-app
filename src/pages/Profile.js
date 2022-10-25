d494f82403e293db5efaddf054889620e86e008a
Marcelo Calsing <marcelocalsing@gmail.com>
2022-10-25 15:52:30 -0300

    Arrumado conflitos de arquivos

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  handleClick = () => {
+    localStorage.clear();
+  };
+
   return (
-    <Header />
+    <div>
+      <Header />
+      <h3 data-testid="profile-email">{localStorage.getItem('user')}</h3>
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">
+          Done Recipes
+
+        </button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button type="button" data-testid="profile-favorite-btn">
+          Favorite Recipes
+
+        </button>
+      </Link>
+      <Link exact to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleClick }
+        >
+          Logout
+
+        </button>
+      </Link>
+      <Footer />
+    </div>
   );
 }
 
