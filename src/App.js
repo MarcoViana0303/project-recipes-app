0eab80f7f992fb2b1057ba2217e85098029121b4
Marcelo Calsing <marcelocalsing@gmail.com>
2022-10-25 15:29:30 -0300

    Requisito 7

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './pages/Profile';
+import Logintest from './pages/Logintest';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Logintest } />
+        <Route exact path="/meals" component={ Meals } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
     </div>
   );
 }
