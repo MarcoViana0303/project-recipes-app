254a602a5c3e9a38d0090b2b68db0bc950163c2f
queones <queones@gmail.com>
2022-10-26 17:26:46 -0300

    Requisito 11 - criada a lógica de pesquisa

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 
 function SearchBar() {
+  const { dataDrinks, dataMeals,
+    requestDrinksAPI, requestMealsAPI, searchInput } = useContext(AppContext);
+
+  const [radioButton, setradioButton] = useState('');
+
+  const location = useLocation();
+
+  const handleClick = () => {
+    if (location.pathname === '/drinks') {
+      if (radioButton === 'i') {
+        requestDrinksAPI(radioButton, searchInput, 'filter');
+      } else {
+        requestDrinksAPI(radioButton, searchInput, 'search');
+      }
+    } else if (location.pathname === '/meals') {
+      if (radioButton === 'i') {
+        requestMealsAPI(radioButton, searchInput, 'filter');
+      } else {
+        requestMealsAPI(radioButton, searchInput, 'search');
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (searchInput.length > 1 && radioButton === 'f') {
+      global.alert('Your search must have only 1 (one) character');
+    }
+  }, [searchInput]);
+
   return (
     <form>
       <div>
         <input
           type="radio"
-          value="ingrediente"
+          value="i"
           name="search"
           data-testid="ingredient-search-radio"
+          onChange={ ({ target }) => setradioButton(target.value) }
         />
         ingrediente
         <input
           type="radio"
-          value="nome"
+          value="s"
           name="search"
           data-testid="name-search-radio"
+          onChange={ ({ target }) => setradioButton(target.value) }
         />
         nome
         <input
           type="radio"
-          value="letra"
+          value="f"
           name="search"
           data-testid="first-letter-search-radio"
+          onChange={ ({ target }) => setradioButton(target.value) }
         />
         primeira letra
       </div>
-      <button type="button" data-testid="exec-search-btn">Pesquisar</button>
+      <button type="button" data-testid="exec-search-btn" onClick={ handleClick }>
+        Pesquisar
+      </button>
     </form>
   );
 }
